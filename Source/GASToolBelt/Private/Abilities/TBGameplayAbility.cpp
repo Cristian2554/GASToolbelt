843a96868c446f28a4f0fc0c598dafe1df77aa16
Cristian Páez <corpari25@gmail.com>
2024-05-01 21:48:38 +0200

    Added Base Ability
    Added Base ability that can autoactivate on granted.

---
+++ Source/GASToolBelt/Private/Abilities/TBGameplayAbility.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TBGameplayAbility.h"
+#include "AbilitySystemComponent.h"
+
+void UTBGameplayAbility::OnAvatarSet(const FGameplayAbilityActorInfo* actorInfo, const FGameplayAbilitySpec& spec)
+{
+	Super::OnAvatarSet(actorInfo, spec);
+
+	if (bActivateOnGranted)
+	{
+		actorInfo->AbilitySystemComponent->TryActivateAbility(spec.Handle, false);
+	}
+}
---
+++ Source/GASToolBelt/Private/Abilities/TBGameplayAbility.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "TBGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASTOOLBELT_API UTBGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category=Ability)
+	bool bActivateOnGranted;
+	
+	virtual void OnAvatarSet(const FGameplayAbilityActorInfo * actorInfo, const FGameplayAbilitySpec & spec) override;
+};
